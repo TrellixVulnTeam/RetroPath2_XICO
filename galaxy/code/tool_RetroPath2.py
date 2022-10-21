1badd16938ab34d5e755dc887ce7fbe57bd6b72c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 00:27:03 +0000

    Adding tarfile member sanitization to extractall()

--- galaxy/code/tool_RetroPath2.py
+++ galaxy/code/tool_RetroPath2.py
@@ -89,7 +89,26 @@ if __name__ == "__main__":
             logging.debug('Rules file: '+str(rulesfile))
         elif params.rulesfile_format=='tar':
             with tarfile.open(params.rulesfile) as rf:
-                rf.extractall(tmpInputFolder)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(rf, tmpInputFolder)
             out_file = glob.glob(tmpInputFolder+'/*.csv')
             if len(out_file)>1:
                 logging.error('Cannot detect file: '+str(glob.glob(tmpInputFolder+'/*.csv')))
